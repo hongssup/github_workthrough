32f05c2bb77d7fef71b604f619718a7c5e9de698
hongssup <hmk0202@naver.com>
2020-08-10 00:27:32 +0900

    upload

---
+++ github_workthrough/ViewController_2.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController_2.swift
+//  github_workthrough
+//
+//  Created by SeoYeon on 2020/08/10.
+//  Copyright © 2020 SeoYeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController_2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
